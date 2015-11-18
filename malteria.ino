b31c9bb026c6dd1144596c5a8f7ff308a24014b1
Héctor Garcés <hectorgarces@gmail.com>
2015-11-18 01:41:09 +0100

    Update malteria.ino

--- malteria.ino
+++ malteria.ino
@@ -1,35 +1,43 @@
 #include <Wire.h>
 #include <Adafruit_AM2315.h>
+#include <LiquidCrystal_I2C.h>
+
+
+
+#define BACKLIGHT_PIN     13
+#define LCD_LINES     2
+#define LCD_CHARTS    16
+#define LCD_I2C_ADD   0x27
 
 #define ID_I2C_TEMP1  0x11
 #define ID_I2C_TEMP2  0x12
 #define ID_I2C_TEMP3  0x13
 #define ID_I2C_TEMP4  0x14
 
-/*************************************************** 
-  This is an example for the AM2315 Humidity + Temp sensor
-  Designed specifically to work with the Adafruit BMP085 Breakout 
-  ----> https://www.adafruit.com/products/1293
-  These displays use I2C to communicate, 2 pins are required to  
-  interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
+ /****************************************************/
 // Connect RED of the AM2315 sensor to 5.0V
 // Connect BLACK to Ground
 // Connect WHITE to i2c clock - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 5
 // Connect YELLOW to i2c data - on '168/'328 Arduino Uno/Duemilanove/etc thats Analog 4
+/****************************************************/
 
 Adafruit_AM2315 Sensor1, Sensor2, Sensor3, Sensor4;
+LiquidCrystal_I2C lcd(LCD_I2C_ADD, LCD_CHARTS, LCD_LINES);  // Set the LCD I2C address
 
 void setup() 
 {
   Serial.begin(9600);
   Serial.println("AM2315 Test!");
+  
+  // INICIALIZACION DEL LCD POR I2C
+  lcd.init();               // initialize the lcd 
+  lcd.clear();
+  lcd.home ();                   // go home
+  lcd.print("Hello, ARDUINO ");  
+  lcd.setCursor ( 0, 1 );        // go to the next line
+  lcd.print (" WORLD!  ");     
 
+  // INICIALIZACION DE LOS SENSORES
   Sensor1.setI2cAdd(ID_I2C_TEMP1);
   Sensor1.setI2cAdd(ID_I2C_TEMP2);
   Sensor1.setI2cAdd(ID_I2C_TEMP3);
